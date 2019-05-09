5cb340682f76ab116041690769f5588080468dc8
fengdongdong02 <fengdongdong02@meituan.com>
2019-05-09 12:19:16 +0800

    init spring boot project

---
+++ src/main/java/com/beanmr/lab/spring/apply/ApplyApplication.java
@@ -0,0 +1,13 @@
+package com.beanmr.lab.spring.apply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyApplication.class, args);
+	}
+
+}
